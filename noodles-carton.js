e5f942d5ccff7b276c6551d7b28f496fc5ecb1a6
Victor Aderibigbe <aderibigbetoluwani5@gmail.com>
2023-06-02 12:12:08 +0100

    Added a comment

--- noodles-carton.js
+++ noodles-carton.js
@@ -1,3 +1,6 @@
+// This algorithm arranges an x number of noodles in a y number of cartons
+// Given that x = 3 and y = 2;
+
 for (let x = 1; x <= 3; x++) {
     for (let y = 1; y <= 2; y++) {
         console.log(`Carton ${x} - Noodle ${y}`);
